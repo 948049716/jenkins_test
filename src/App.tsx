c5c12ae48af39fa204b4102f44b4b4d6fbf0969f
Miong <wuminghong_rich@icloud.com>
2023-11-30 15:15:14 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div style={{fontSize:'30px',fontWeight:'bold',textAlign:'center'}}>welcome to Miong's website</div>
+    </>
+  )
+}
+
+export default App
